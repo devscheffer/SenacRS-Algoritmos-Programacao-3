c382fbb7bc163297cfb40dc375a3dd6afabd1e8f
devscheffer <devscheffer@gmail.com>
2021-03-17 21:48:08 -0300

    ex2

---
+++ Ex#2.py
@@ -0,0 +1,3 @@
+'''
+Crie uma classe “ContaCorrente” que possui um número, um saldo, um status (informa se é especial ou comum) e um limite. Após, crie uma classe “TestaContaCorrente” onde os atributos são inicializados e mostrados na tela. Desenvolva métodos para realizar saque (verificando se o cliente possui as condições necessárias), depositar, consultar saldo e verificar o uso do cheque especial (ou não).
+'''

679389065d7f0ccef4c6368069a34a2815e5e8c4
devscheffer <devscheffer@gmail.com>
2021-03-19 19:30:37 -0300

    [done] ex3

--- Ex_3.py
+++ Ex_3.py
@@ -1,14 +1,24 @@
 '''
-# [todo]#1
 Escreva uma classe Aluno com os atributos relacionados às principais características de um aluno
 - nome
 - matrícula
 - curso
 - média desse aluno.
-'''
-'''
-# [todo]#2
 Desenvolva os seguintes métodos:
 - verificar se o aluno está aprovado (média maior ou igual a 7).
 '''
 
+
+class cls_Aluno:
+    def __init__(self, nome, matricula, curso, media):
+        self.nome = nome
+        self.matricula = matricula
+        self.curso = curso
+        self.media = media
+
+    def mtd_check_is_approved(self):
+        if self.media >= 7:
+            self.status = 'aprovado'
+        else:
+            self.status = 'reprovado'
+        print(f'Aluno foi {self.status} na matéria')

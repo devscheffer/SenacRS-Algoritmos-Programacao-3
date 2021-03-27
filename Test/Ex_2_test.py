df7d95e77b99a97e2b284268d93cf3ce3e3bbf38
devscheffer <devscheffer@gmail.com>
2021-03-26 21:35:03 -0300

    correcao get

--- Test/Ex_2_test.py
+++ Test/Ex_2_test.py
@@ -2,14 +2,14 @@ from main.Ex_2 import cls_Conta_Corrente, cls_Testa_Conta_Corrente
 
 def test_cls():
 	res = cls_Testa_Conta_Corrente(cls_Conta_Corrente(1, 10, "especial", 200))
-	assert res.conta.saldo == 10
+	assert res.conta.get_saldo() == 10
 
 def test_saque():
 	res = cls_Testa_Conta_Corrente(cls_Conta_Corrente(1, 10, "especial", 200))
 	res.mtd_saque(5)
-	assert res.conta.saldo == 5
+	assert res.conta.get_saldo() == 5
 
 def test_deposito():
 	res = cls_Testa_Conta_Corrente(cls_Conta_Corrente(1, 10, "especial", 200))
 	res.mtd_depositar(70)
-	assert res.conta.saldo == 80
+	assert res.conta.get_saldo() == 80

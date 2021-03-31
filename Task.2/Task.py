3073fd02a8d4b7eb7ac4eaf14f255a751673add4
devscheffer <devscheffer@gmail.com>
2021-03-31 20:01:46 -0300

    inclusao classe task.2

--- Task.2/Task.py
+++ Task.2/Task.py
@@ -1,25 +1,28 @@
-def fn_check_ano(ano):
-	if len(ano)!=4:
-		return ano
-	else:
-		new_input=input('Tente novamente o ano')
-		return fn_check_ano(new_input)
+class cls_data:
+	def __init__(self,dia,mes,ano):
+		self.dia=dia
+		self.mes=mes
+		self.ano=ano
+
+	def fn_check_ano(self):
+		if len(self.ano)!=4:
+			return self.ano
+		else:
+			self.ano=input('Tente novamente o ano')
+			return fn_check_ano(self)
+
+	def fn_check_mes(self):
+		if int(mes) in range(1,13):
+			return mes
+		else:
+			self.mes=input('Tente novamente o mes')
+			return fn_check_mes(self)
+
+	def fn_check_dia(self):
+		if int(dia) in range(1,31):
+			return dia
+		else:
+			self.dia=input('Tente novamente o dia')
+			return fn_check_dia(self)
 
 
-def fn_check_mes(mes):
-	if int(mes) in range(1,13):
-		return mes
-	else:
-		new_input=input('Tente novamente o mes')
-		return fn_check_mes(new_input)
-
-def fn_check_dia(dia):
-	if int(dia) in range(1,31):
-		return dia
-	else:
-		new_input=input('Tente novamente o dia')
-		return fn_check_dia(new_input)
-
-ano=fn_check_ano(input('ano'))
-mes=fn_check_mes(input('mes'))
-dia=fn_check_dia(input('dia'))

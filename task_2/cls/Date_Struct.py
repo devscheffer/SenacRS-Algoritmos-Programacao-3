45901456d94c9cc7408f4bac2c1a7c000428035e
devscheffer <devscheffer@gmail.com>
2021-04-02 23:17:10 -0300

    [method] acrescentarDias

--- task_2/cls/Date_Struct.py
+++ task_2/cls/Date_Struct.py
@@ -1,44 +1,43 @@
 import os,sys
-cwd=os.getcwd()
+cwd = os.getcwd()
 sys.path.append(cwd)
+from task_2.fn.fn_aux import fn_check_ano, fn_check_mes, fn_check_dia
 
-from task_2.fn.fn_aux import fn_check_ano,fn_check_mes,fn_check_dia
 
 class cls_Date_Struct:
-	def __init__(self,dia:int,mes:int,ano:int):
-		self.dia=dia
-		self.mes=mes
-		self.ano=ano
+	def __init__(self, dia: int, mes: int, ano: int):
+		self.dia = dia
+		self.mes = mes
+		self.ano = ano
 
 	@property
 	def dia(self):
 		return self.__dia
 
 	@dia.setter
-	def dia(self,value):
-		dia_new=fn_check_dia(value)
-		self.__dia=dia_new
+	def dia(self, value: int) -> int:
+		dia_new = fn_check_dia(value)
+		self.__dia = dia_new
 
 	@property
 	def mes(self):
 		return self.__mes
 
 	@mes.setter
-	def mes(self,value):
-		mes_new=fn_check_mes(value)
-		self.__mes=mes_new
+	def mes(self, value: int) -> int:
+		mes_new = fn_check_mes(value)
+		self.__mes = mes_new
 
 	@property
 	def ano(self):
 		return self.__ano
 
 	@ano.setter
-	def ano(self,value):
-		ano_new=fn_check_ano(value)
-		self.__ano=ano_new
+	def ano(self, value: int) -> int:
+		ano_new = fn_check_ano(value)
+		self.__ano = ano_new
 
-
-	def mtd_Inicializar_Data(self)->str:
+	def mtd_Inicializar_Data(self) -> int:
 		print(f'Ano: {self.ano}')
 		print(f'Mes: {self.mes}')
 		print(f'Dia: {self.dia}')
@@ -47,4 +46,16 @@ class cls_Date_Struct:
 		print(data)
 		return data
 
+	def mtd_Acrescentar_Dias(self, value:int):
+		"""Adiciona quantidade de dias a data
 
+		Args:
+			value (int): [description]
+		"""
+		ano_dias = 31*12
+		dia = (value % ano_dias) % 31
+		mes = (value % ano_dias)//31
+		ano = value//ano_dias
+		self.dia = self.dia+dia
+		self.mes = self.mes+mes
+		self.ano = self.ano+ano
--- task_2/test/ex1_test.py
+++ task_2/test/ex1_test.py
@@ -1,25 +1,54 @@
-import pytest
 from task_2.cls.Date_Struct import cls_Date_Struct
-
-def test_ano():
-    with pytest.raises(ValueError):
-        cls_Date_Struct(1, 2, 1)
-
-
-def test_mes():
-    with pytest.raises(ValueError):
-        cls_Date_Struct(1, 22, 2020)
+import pytest
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(cwd)
 
 
-def test_dia():
-    with pytest.raises(ValueError):
-        cls_Date_Struct(-1, 2, 2020)
+class Test_cls_init:
+	def test_class_ini(self):
+		res = cls_Date_Struct(1, 2, 1111)
+		assert res.ano == 1111
+		assert res.mes == 2
+		assert res.dia == 1
 
-def test_class_ini():
-	res=cls_Date_Struct(1,2,1111)
-	assert res.ano == 1111
-	assert res.mes == 2
-	assert res.dia == 1
-def test_mtd_1():
-	res=cls_Date_Struct(1,1,1111)
-	assert res.mtd_Inicializar_Data() == 11110101
+	def test_ano(self):
+		with pytest.raises(ValueError):
+			cls_Date_Struct(1, 2, 1)
+
+	def test_mes(self):
+		with pytest.raises(ValueError):
+			cls_Date_Struct(1, 22, 2020)
+
+	def test_dia(self):
+		with pytest.raises(ValueError):
+			cls_Date_Struct(-1, 2, 2020)
+
+
+class Test_mtd_Inicializar_Data:
+	def test_return(self):
+		res = cls_Date_Struct(1, 1, 1111)
+		assert res.mtd_Inicializar_Data() == 11110101
+
+
+class Test_mtd_Acrescentar_Dias:
+
+	def test_add_dia_lt_31(self):
+		res = cls_Date_Struct(1, 1, 1111)
+		res.mtd_Acrescentar_Dias(5)
+		assert res.dia == 6
+
+	def test_add_dia_gt_31(self):
+		res = cls_Date_Struct(1, 1, 1111)
+		res.mtd_Acrescentar_Dias(400)
+		assert res.ano == 1112
+		assert res.mes == 1
+		assert res.dia == 29
+
+	def test_add_negative(self):
+		res = cls_Date_Struct(1, 1, 1111)
+		res.mtd_Acrescentar_Dias(-40)
+		assert res.ano == 1110
+		assert res.mes == 11
+		assert res.dia == 23

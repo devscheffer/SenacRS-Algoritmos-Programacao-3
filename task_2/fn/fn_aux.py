8f4a6c5adcabec720f8eb6ca77374ca79f5350ac
devscheffer <devscheffer@gmail.com>
2021-04-04 11:09:26 -0300

    [wip]adicionar dias

--- task_2/fn/fn_aux.py
+++ task_2/fn/fn_aux.py
@@ -36,82 +36,58 @@ def fn_mes_int_to_str(value:int)->str:
 }
 	return dct_mes[value]
 
+
 def fn_leap(ano)->int:
 	ano=ano
 	res = ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0)
 	return int(res)
 
-def fn_dias_mes(mes:int, ano:int)->int:
+def fn_days_in_year(ano:int)->int:
+	return 365+fn_leap(ano)
+
+def fn_days_in_month(mes:int, ano:int)->int:
     if mes in {1, 3, 5, 7, 8, 10, 12}:
         return 31
     if mes == 2:
-        if fn_leap(ano):
-            return 29
-        return 28
+        return 28+fn_leap(ano)
     return 30
 
-def fn_dias_ano(ano:int)->int:
-	total=0
-	for i in range(1,13):
-		total=total+fn_dias_mes(i,ano)
-	return total
-def fn_dias_passado_ano(ano,mes,dia):
-	total=0
-	for i in range(1,mes+1):
-		total += fn_dias_mes(i,ano)
-	total=total-fn_dias_mes(mes,ano)+dia
-	return total
+def fn_days_passed_in_year(ano,mes,dia):
+	days_passed=0
+	for i in range(1,mes):
+		days_passed += fn_days_in_month(i,ano)
+	days_passed+=dia
+	return days_passed
 
-def fn_date_add(dias:int,ano:int,mes:int)->list:
-	# ano=2020
-	# mes=2
-	# dia=1
-	# dias=366
-	ano=ano
-	mes=mes
-	dia=1
-	dias=dias
-	add_dia = dias
-	add_ano=0
+'''
+[todo]: Corrigir adiconar data
+Usar dias passado + dias > 0
+se negativo
+se positivo
+'''
 
 
-	dias_ano=fn_dias_ano(ano+add_ano)
-	while add_dia+fn_dias_passado_ano(ano,mes,dia) > dias_ano:
-		dias_ano=fn_dias_ano(ano+add_ano)
-		if add_dia+fn_dias_passado_ano(ano,mes,dia) > dias_ano:
-			add_dia = add_dia-dias_ano+fn_dias_passado_ano(ano,mes,dia)
-			add_ano+=1
-	add_mes=0
-	dias_mes=fn_dias_mes(mes+add_mes,ano+add_ano)
-	while add_dia > dias_mes:
-		dias_mes=fn_dias_mes(mes+add_mes,ano+add_ano)
-		if add_dia > dias_mes:
-			add_dia = add_dia-dias_mes
-			add_mes+=1
-	return [ano+add_ano,mes+add_mes,dia+add_dia]
-# fn_date_add(1,1,1)
+def fn_days_to_add(days_to_add:int,year:int,month:int,day:int)->list:
+	days_rest=fn_days_passed_in_year(year,month,day)+days_to_add
+	add_year=0
+	if  days_rest > fn_days_in_year(year+add_year) and days_rest>0:
+		days_rest -= fn_days_in_year(year)
+		add_year +=1
+	if abs(days_rest)>fn_days_in_year(year+add_year) and days_rest<=0:
+		days_rest += fn_days_in_year(year)
+		add_year -=1
 
-def fn_date_sub(ano:int,mes:int,dia:int,dias:int)->list:
-	ano=ano
-	mes=mes
-	dia=dia
-	dias=dias
-	# ano=2020
-	# mes=1
-	# dia=1
-	# dias=-1
-	sub_ano=0
-	dias_passados=fn_dias_passado_ano(ano,mes,dia)
-	while abs(dias) >= dias_passados:
-		if abs(dias) >= dias_passados:
-			dias=dias + dias_passados
-			sub_ano+=1
-	dias_total=fn_dias_ano(ano-sub_ano)+dias
+	add_month=0
+	days_rest=days_rest
+	if days_rest > fn_days_in_month(month+add_month,year+add_year) and days_rest>0:
+		days_rest -= fn_days_in_month(month+add_month,year+add_year)
+		add_month+=1
+	if abs(days_rest) > fn_days_in_month(month+add_month,year+add_year) and days_rest<=0:
+		days_rest += fn_days_in_month(month+add_month,year+add_year)
+		add_month-=1
 
-	lista=fn_date_add(dias_total,ano,mes)
-	a=lista[0]
-	sub_mes=lista[1]
-	sub_dia=lista[2]
-	return [ano-sub_ano,sub_mes,sub_dia]
+	add_day=days_rest-fn_days_passed_in_year(year,month,day)
+	return [year+add_year,month+add_month,day+add_day]
 
-print(fn_date_sub(1,1,1,1))
+print(fn_days_to_add(5,2020,1,1))
+

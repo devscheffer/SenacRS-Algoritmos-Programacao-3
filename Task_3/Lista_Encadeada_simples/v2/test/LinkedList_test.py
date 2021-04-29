4702ebf57194d9d5da9ba8c295a2668a5b12c2ae
devscheffer <devscheffer@gmail.com>
2021-04-28 21:55:00 -0300

    testes de remocao de node

--- Task_3/Lista_Encadeada_simples/v2/test/LinkedList_test.py
+++ Task_3/Lista_Encadeada_simples/v2/test/LinkedList_test.py
@@ -10,16 +10,38 @@ class Test_linkedList:
 		lista = LinkedList()
 		lista.Node_add('Eduarda', 0)
 		assert lista.viewList() == ['Eduarda']
+		assert lista.len_list == 1
 
 	def test_index_lt_0(self):
 		lista = LinkedList()
 		with pytest.raises(ValueError):
 			lista.Node_add('Eduarda', -1)
 
-	def test_many(self):
+	def test_add(self):
 		lista = LinkedList()
 		lista.Node_add('p1', 0)
 		lista.Node_add('p2', 1)
 		lista.Node_add('p3', 2)
 		lista.Node_add('p4', 5)
 		assert lista.viewList() == ['p1','p2','p3','p4']
+		assert lista.len_list == 4
+
+	def test_sub_by_index(self):
+		lista = LinkedList()
+		lista.Node_add('p1', 0)
+		lista.Node_add('p2', 1)
+		lista.Node_add('p3', 2)
+		lista.Node_add('p4', 5)
+		lista.Node_remove_by_index(0)
+		assert lista.viewList() == ['p2','p3','p4']
+		assert lista.len_list == 3
+
+	def test_sub_by_value(self):
+		lista = LinkedList()
+		lista.Node_add('p1', 0)
+		lista.Node_add('p2', 1)
+		lista.Node_add('p3', 2)
+		lista.Node_add('p4', 5)
+		lista.Node_remove_by_label('p1')
+		assert lista.viewList() == ['p2','p3','p4']
+		assert lista.len_list == 3
